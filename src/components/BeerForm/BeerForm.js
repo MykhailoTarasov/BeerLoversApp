e97b8d76ce96f3478a65bfd647cabbb44beb57a9
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-30 16:08:10 +0200

    added globalstyled

--- src/components/BeerForm/BeerForm.js
+++ src/components/BeerForm/BeerForm.js
@@ -1,4 +1,32 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validation = Yup.object().shape({
+    beer: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('This is a required field!'),
+
+     place: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('This is a required field!'),
+
+     date: Yup.string()
+     .min(4, 'Too Short!')
+     .max(10, 'Too Long!')
+     .required('This is a required field!'),
+
+     brewery: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('This is a required field!'),
+
+     style: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('This is a required field!'),
+  });
 
 
 const BeerForm = ({onAdd}) => {
@@ -11,7 +39,7 @@ const BeerForm = ({onAdd}) => {
         brewery: '',
         style: '',
       }}
-     
+      validationSchema={validation}
       onSubmit={(values, actions) => {
         onAdd(values);
         actions.resetForm();
@@ -20,22 +48,27 @@ const BeerForm = ({onAdd}) => {
       <Form>
         <label>
           <Field id="beer" name="beer" placeholder="Beer" />
+          <ErrorMessage name="beer"/>
         </label>
 
         <label>
           <Field id="place" name="place" placeholder="Place" />
+          <ErrorMessage name="place"/>
         </label>
 
         <label>
           <Field id="date" name="date" placeholder="Date" />
+          <ErrorMessage name="date"/>
         </label>
 
         <label>
           <Field id="brewery" name="brewery" placeholder="Brewery" />
+          <ErrorMessage name="brewery"/>
         </label>
 
         <label>
-          <Field id="style" name="style" placeholder="IPA" />
+          <Field id="style" name="style" placeholder="Style" />
+          <ErrorMessage name="style"/>
         </label>
 
         <button type="submit">Add beer</button>
