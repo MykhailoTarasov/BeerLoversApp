698327543bd97f216ea78aceb2c10d05b571f950
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-30 13:01:23 +0200

    started with a form

--- src/components/BeerForm/BeerForm.js
+++ src/components/BeerForm/BeerForm.js
@@ -1,7 +1,42 @@
+import { Formik, Field, Form } from 'formik';
+
 const BeerForm = () => {
-    return <div>
-        <h3>BeerForm</h3>
-    </div>
-}
+  return (
+    <Formik
+      initialValues={{
+        beer: '',
+        place: '',
+        date: '',
+        brewery: '',
+        style: '',
+      }}
+      onSubmit={values => {}}
+    >
+      <Form>
+        <label>
+          <Field id="beer" name="beer" placeholder="Beer" />
+        </label>
+
+        <label>
+          <Field id="place" name="place" placeholder="Place" />
+        </label>
+
+        <label>
+          <Field id="date" name="date" placeholder="Date" />
+        </label>
+
+        <label>
+          <Field id="brewery" name="brewery" placeholder="Brewery" />
+        </label>
+
+        <label>
+          <Field id="style" name="style" placeholder="IPA" />
+        </label>
+
+        <button type="submit">Add beer</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default BeerForm;
